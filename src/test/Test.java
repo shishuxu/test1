d841096cdeaa474a9e3acf9bf7aed73f487c4c38
shishuxu <xu457683048@163.com>
2018-08-25 22:07:40 +0800

    abc

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,8 +1,8 @@
 package test;
 
-public class Main {
+public class Test {
 
     public static void main(String[] args) {
-        System.out.println("sss");
+        System.out.println(1256585555);
     }
 }
